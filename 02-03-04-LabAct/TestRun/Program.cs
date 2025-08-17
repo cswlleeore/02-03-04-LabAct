fc6817bd02d79e14c042dbd876db47278f93e2ad
bernzDEV <142583053+bernzDEV@users.noreply.github.com>
2025-08-17 14:08:57 +0800

    Add files via upload

---
+++ 02-03-04-LabAct/TestRun/Program.cs
@@ -0,0 +1,11 @@
+﻿
+class Program {
+
+    static void Main(string[] args)
+    {
+        //Change the name to run your code
+        const string name = "Template";
+        StudentSelector.RunSelectedStudent(name);
+    }
+
+}
---
+++ 02-03-04-LabAct/TestRun/Student/Template.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRun.Student
+{
+    internal class Template : Student
+    {
+        public override void Run()
+        {
+            Console.WriteLine("Running Template's code...");
+            
+        }
+   
+    }
+}
+//THIS IS A TEMPLATE FILE 
